1b7291ae52eb0ea3299f0933da317a493f1227d6
Vatsal Chauhan <112819416+Vatsal-Chauhan024@users.noreply.github.com>
2024-07-19 16:00:42 +0530

    frontend deploy
    Frontend Structure is ready.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import Signup from './pages/Signup'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+import Header from './components/Header'
+
+
+function App() {
+
+  return (
+    <>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path='/' element = {<Home/>}/>
+      <Route path='/about' element = {<About/>}/>
+      <Route path='/sign-in' element = {<SignIn/>}/>
+      <Route path='/sign-up' element = {<Signup/>}/>
+      <Route path='/dashboard' element = {<Dashboard/>}/>
+      <Route path='/projects' element = {<Projects/>}/>
+    </Routes>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { Button, Navbar, TextInput } from "flowbite-react";
+import { Link, useLocation } from "react-router-dom";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa";
+
+const Header = () => {
+  const path = useLocation().pathname;
+
+  return (
+    <Navbar className="border-b-2">
+      <Link
+        to="/"
+        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+      >
+        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+          Vatsal
+        </span>
+        Blog
+      </Link>
+
+      <form action="">
+        <TextInput
+          type="text"
+          placeholder="Search..."
+          rightIcon={AiOutlineSearch}
+          className="hidden lg:inline"
+        />
+      </form>
+
+      <Button className="w-12 h-10 lg:hidden" color="gray" pill>
+        <AiOutlineSearch className="text-lg" />
+      </Button>
+
+      <div className="flex gap-2 md:order-2">
+        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
+          <FaMoon />
+        </Button>
+
+        <Link to="/sign-in">
+          <Button gradientDuoTone="purpleToBlue">SignIn</Button>
+        </Link>
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse className="">
+        {["/", "about", "projects"].map((elem, i) => (
+          <Navbar.Link
+            className="*:text-sm *:sm:text-base"
+            active={i === 0 ? path === "/" : path === "/" + elem.toLowerCase()}
+            as="div"
+            key={i}
+          >
+            <Link to={i === 0 ? "/" : `/${elem.toLowerCase()}`}>
+              {elem === "/" ? "Home" : elem.toUpperCase()}
+            </Link>
+          </Navbar.Link>
+        ))}
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default Header;
