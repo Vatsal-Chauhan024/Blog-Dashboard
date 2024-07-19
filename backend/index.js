b95d7e6828d42b8f8f40971b1acb9f966fb3303e
Vatsal Chauhan <112819416+Vatsal-Chauhan024@users.noreply.github.com>
2024-07-19 17:04:12 +0530

    test api routes

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
 import express from "express"
 const app = express()
 import "./db/connnect.js"
+import cors from "cors"
+import UserRoutes from "./routes/UserRoutes.js"
 
+app.use(cors())
+app.use("/api/user", UserRoutes)
 
 app.listen(3000, () => {
     console.log(`Server is running on: 3000` )
