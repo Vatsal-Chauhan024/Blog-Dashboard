a51a6fbadde2db9315223e63a0efcad092722856
Vatsal Chauhan <112819416+Vatsal-Chauhan024@users.noreply.github.com>
2024-07-20 17:34:47 +0530

    Sidebar Dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashboardProfile from "../components/DashboardProfile"
+import DashboardSidebar from "../components/DashboardSidebar";
 
 const Dashboard = () => {
-  return (
-    <div>
-      Dashboard Page
-    </div>
-  )
-}
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParam = new URLSearchParams(location.search);
+    const tabFromUrl = urlParam.get("tab");
+    if(tabFromUrl){
+      setTab(tabFromUrl)
+    }
+  }, [location.search]);
 
-export default Dashboard
+  return (
+    <div className="min-h-screen flex flex-col md:flex-row">
+      {/* Side bar  */}
+      <div className="md:w-56">
+      <DashboardSidebar/>
+      </div>
+      {tab === "profile" && <DashboardProfile/>}
+    </div>
+  );
+};
+
+export default Dashboard;
