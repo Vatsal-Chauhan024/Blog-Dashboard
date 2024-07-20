3b5e9b1e0d80e068c8147953ca28cd37d3cd6583
Vatsal Chauhan <112819416+Vatsal-Chauhan024@users.noreply.github.com>
2024-07-20 14:20:30 +0530

    SignIn Page commit

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,11 +1,121 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 
-const SignIn = () => {
+const Signin = () => {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!formData.email || !formData.password) {
+      return setErrorMessage("Fields are Empty!!");
+    }
+
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+      if (data.success === false) {
+        setLoading(false);
+        return setErrorMessage(data.message);
+      }
+      navigate("/");
+      setLoading(false);
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
+
   return (
-    <div>
-      SignIn Page
-    </div>
-  )
-}
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-4xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        <div className="flex-1">
+          <Link to="/" className="font-bold dark:text-white text-4xl">
+            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+              Vatsal
+            </span>
+            Blog
+          </Link>
 
-export default SignIn
+          <p className="text-sm mt-5">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas
+            adipisci suscipit temporibus ipsam cum quidem facilis assumenda
+            aliquam. Pariatur, omnis.
+          </p>
+        </div>
+
+        <div className="flex-1">
+          <form
+            action=""
+            className="flex flex-col gap-4"
+            onSubmit={handleSubmit}
+          >
+            <div>
+              <Label value="Email" />
+              <TextInput
+                type="email"
+                placeholder="Enter Your Email"
+                id="email"
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <Label value="Password" />
+              <TextInput
+                type="password"
+                placeholder="Enter Your Password"
+                id="password"
+                onChange={handleChange}
+              />
+            </div>
+
+            <Button
+              gradientDuoTone="purpleToPink"
+              type="submit"
+              disabled={loading}
+            >
+              {loading ? (
+                <div className="flex items-center justify-around">
+                  <Spinner size="sm" />
+                  <span className="p-3">Loading....</span>
+                </div>
+              ) : (
+                "SignIn"
+              )}
+            </Button>
+          </form>
+
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Don't have an account? </span>
+            <Link to="/sign-up" className="text-blue-500 underline">
+              Sign in
+            </Link>
+          </div>
+
+          {errorMessage && (
+            <Alert className="mt-5" color="failure">
+              {errorMessage}
+            </Alert>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signin;
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -111,7 +111,7 @@ const Signup = () => {
 
           <div className="flex gap-2 text-sm mt-5">
             <span>Have an account</span>
-            <Link to="sign-in" className="text-blue-500 underline">
+            <Link to="/sign-in" className="text-blue-500 underline">
               Sign in
             </Link>
           </div>
