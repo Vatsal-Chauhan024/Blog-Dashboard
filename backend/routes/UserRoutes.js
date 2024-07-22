f2faadac347e53860955506ec5c0b737277eae4a
Vatsal Chauhan <112819416+Vatsal-Chauhan024@users.noreply.github.com>
2024-07-22 18:15:13 +0530

    getAll Users

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { updateUser, deleteUser, signout } from "../controllers/UserController.js"
+import { updateUser, deleteUser, signout, getUsers } from "../controllers/UserController.js"
 import { verifyToken } from "../utils/VerifyUser.js"
 const router = express.Router()
 
@@ -8,5 +8,6 @@ const router = express.Router()
 router.put("/update/:userId",verifyToken, updateUser)
 router.delete("/delete/:userId", verifyToken, deleteUser)
 router.post("/signout", signout)
+router.get("/getusers", verifyToken, getUsers)
 
 export default router
