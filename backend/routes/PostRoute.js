34da93efb6b2ce16b1a5ce212e02279e07e1f66f
Vatsal Chauhan <112819416+Vatsal-Chauhan024@users.noreply.github.com>
2024-07-22 15:50:04 +0530

    Get Post Api Route

--- backend/routes/PostRoute.js
+++ backend/routes/PostRoute.js
@@ -1,10 +1,11 @@
 import express from "express"
 import {verifyToken} from "../utils/VerifyUser.js"
-import { createPost } from "../controllers/PostController.js"
+import { createPost, getposts } from "../controllers/PostController.js"
 const router = express.Router()
 
 
 router.post("/create", verifyToken, createPost)
+router.get("/getposts", getposts)
 
 
 export default router
--- frontend/src/components/DashboardPosts.jsx
+++ frontend/src/components/DashboardPosts.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const DashboardPosts = () => {
   return (
     <div>
-      
+      Posts
     </div>
   )
 }
