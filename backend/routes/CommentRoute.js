473413bbc48ac6fa82f837e2883d423f38228f29
Vatsal Chauhan <112819416+Vatsal-Chauhan024@users.noreply.github.com>
2024-07-23 19:41:39 +0530

    Edit Functionality for Comment.

--- backend/routes/CommentRoute.js
+++ backend/routes/CommentRoute.js
@@ -1,11 +1,12 @@
 import express from "express"
 import {verifyToken} from "../utils/VerifyUser.js"
-import { createComment, getComments, likeComment} from "../controllers/CommentController.js";
+import { createComment, getComments, likeComment, editComment} from "../controllers/CommentController.js";
 const router = express.Router();
 
 
 router.post("/create",verifyToken, createComment)
 router.get("/getcomments/:postId", getComments)
 router.put("/likeComment/:commentId", verifyToken, likeComment)
+router.put("/editComment/:commentId", verifyToken, editComment)
 
 export default router;
--- frontend/src/components/CommentSection.jsx
+++ frontend/src/components/CommentSection.jsx
@@ -84,7 +84,13 @@ const CommentSection = ({ postId }) => {
   };
 
 
-
+  const handleEdit = async (comment, editedContent) => {
+    setComments(
+      comments.map((c) =>
+        c._id === comment._id ? { ...c, content: editedContent } : c
+      )
+    );
+  };
 
   return (
     <div className='max-w-2xl mx-auto w-full p-3'>
@@ -153,6 +159,7 @@ const CommentSection = ({ postId }) => {
               key={comment._id}
               comment={comment}
               onLike={handleLike}
+              onEdit={handleEdit}
             />
           ))}
         </>
