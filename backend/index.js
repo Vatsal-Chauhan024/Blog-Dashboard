0d870de22fd24f747fc91615fb0844ae21b68380
Vatsal Chauhan <112819416+Vatsal-Chauhan024@users.noreply.github.com>
2024-07-24 22:11:58 +0530

    add pre Installation for render

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,9 @@ import PostRoute from "./routes/PostRoute.js"
 import CommentRoute from "./routes/CommentRoute.js"
 import cookieParser from "cookie-parser"
 
+import path from "path"
+const __dirname = path.resolve();
+
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors())
@@ -15,6 +18,11 @@ app.use("/api/post", PostRoute)
 app.use("/api/user", UserRoutes)
 app.use("/api/auth", AuthRoutes)
 app.use("/api/comment", CommentRoute)
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
